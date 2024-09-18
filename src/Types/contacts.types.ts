f391ca4026787cb60d0f922d736ef66a14d22bce
Adham Usama <adhamusama25@gmail.com>
2024-09-18 15:15:56 +0300

    update

--- src/Types/contacts.types.ts
+++
@@ -1,8 +0,0 @@
-import { IconDefinition } from "@fortawesome/free-brands-svg-icons";
-
-export default interface IContact {
-  name: string;
-  value: string;
-  url: string;
-  icon: IconDefinition;
-}
--- src/Types/navigation.types.ts
+++
@@ -1,4 +0,0 @@
-export default interface INavigation {
-    title: string;
-    link: string;
-}
